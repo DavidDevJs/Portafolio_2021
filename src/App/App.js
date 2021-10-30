1cbd39916006d2ff0fa4366fe21cb047153f6d63
David <davidgonzalez0519@gmail.com>
2021-10-30 16:38:54 -0500

    nav in progres

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import logo from "./assets/Icon.svg";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <img className="Icon" src={logo} alt="Logo David Gonzalez" />
+        <h1>Front-end Developer</h1>
+      </nav>
+    </>
+  );
+}
+
+export default App;
