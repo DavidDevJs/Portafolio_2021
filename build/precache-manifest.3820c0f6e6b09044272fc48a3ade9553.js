6053ecbe484c6d0ce3bd5416f05010cf251802f3
David Gonzalez <davidgonzalez0519@gmail.com>
2021-12-06 17:49:30 -0500

    test build

---
+++ build/precache-manifest.3820c0f6e6b09044272fc48a3ade9553.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e104ea229fc11066b4633971368d8b99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe74a2443a357858df4",
+    "url": "/static/css/main.84c1c738.chunk.css"
+  },
+  {
+    "revision": "1dec6a1ce65d41688b8b",
+    "url": "/static/js/2.f597c480.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.f597c480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe74a2443a357858df4",
+    "url": "/static/js/main.f20c95a1.chunk.js"
+  },
+  {
+    "revision": "3b20209cdfdd6b932b21",
+    "url": "/static/js/runtime-main.6ff17799.js"
+  },
+  {
+    "revision": "fe6e0fcc89df8c5df232ec6bfb9ef9f3",
+    "url": "/static/media/imgProfile.fe6e0fcc.jpg"
+  },
+  {
+    "revision": "e0a8e3fb687b5666364d629349c4aa1e",
+    "url": "/static/media/loginTwitter.e0a8e3fb.gif"
+  }
+]);
