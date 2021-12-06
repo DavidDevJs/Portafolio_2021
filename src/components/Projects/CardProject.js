02b9e01f15869e9f6f93a5d245491c286dcbb1f7
David Gonzalez <davidgonzalez0519@gmail.com>
2021-12-06 08:37:27 -0500

    Layout projects done

--- src/components/Projects/CardProject.js
+++ src/components/Projects/CardProject.js
@@ -4,14 +4,7 @@ const CardProject = ({ children }) => {
   return (
     <div className="project__container">
       <li>
-        <div className="project__item">
-          <img
-            className="project__item--img"
-            src="https://images.unsplash.com/photo-1571028634586-ae87c1a42432?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=386&q=80"
-            alt="test"
-          />
-          {children}
-        </div>
+        <div className="project__item">{children}</div>
       </li>
     </div>
   );
