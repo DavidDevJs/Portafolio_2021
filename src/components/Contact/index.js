f80b56d9e22de5758b343a9eba18959d1c244f24
David Gonzalez <davidgonzalez0519@gmail.com>
2021-12-07 06:24:43 -0500

    form contact in progress

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
+  const handleSubmit = (event) => {};
+
   return (
     <section className="contact" id="contact">
       <h1 className="contact__title">Contact</h1>
       <div className="contact-wrapper">
         <div className="contact-form">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="contact-form__item">
               <label>FullName</label>
-              <input type="text" name="fullname" />
+              <input type="text" name="fullname" required />
             </div>
             <div className="contact-form__item">
               <label>Email Adress</label>
-              <input type="email" name="email" />
+              <input type="email" name="email" required />
             </div>
             <div className="contact-form__item message">
               <label>Message</label>
