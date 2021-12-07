c1d50d27f7a6c87e0950048a0061ee42becca979
David Gonzalez <davidgonzalez0519@gmail.com>
2021-12-07 16:24:18 -0500

    Fix bug getDataProjects

--- src/components/Projects/ProjectsList.js
+++ src/components/Projects/ProjectsList.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
-
-import { Description } from "./Description";
-import getDataProjects from "./getData";
-
+import React, { useState, useEffect } from "react";
 import "./projects.css";
 import "./cardProject.css";
+import { Description } from "./Description";
+import { collection, getDocs } from "@firebase/firestore";
+import db from "../../firebase/firebaseConfig";
 
 const ProjectsList = () => {
-  const [projects, setProjects] = useState(getDataProjects());
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await getDocs(collection(db, "projects"));
+        data.forEach((doc) => {
+          const dataProjects = { ...doc.data(), id: doc.id };
+          setProjects([...projects, dataProjects]);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getData();
+  }, []);
+
   console.log(projects);
-  projects.forEach((i) => console.log(i));
   return (
     <section className="projects__list">
       <ul className="projects__list--ul"></ul>
