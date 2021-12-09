4b37a62d779905701a2c009e123d73d5da6a308f
David Gonzalez <davidgonzalez0519@gmail.com>
2021-12-09 18:41:35 -0500

    Rename [hash].html to index.html

--- webpack.prod.js
+++ webpack.prod.js
@@ -50,7 +50,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
-      filename: "./[name].[contenthash].html",
+      filename: "index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
